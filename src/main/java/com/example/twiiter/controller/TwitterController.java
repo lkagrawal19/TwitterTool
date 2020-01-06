0e052271d464defa61eb1adda0b6de2d33984e4f
Lokesh <lokesh.agrawal@freshworks.com>
2020-01-07 00:05:27 +0530

    Allowing cors for local

--- src/main/java/com/example/twiiter/controller/TwitterController.java
+++ src/main/java/com/example/twiiter/controller/TwitterController.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -19,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class TwitterController {
 
     private static final Logger logger = LoggerFactory.getLogger(TwitterController.class);
